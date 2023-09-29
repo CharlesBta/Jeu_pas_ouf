5923da3162e6a8c1fe1b9d38888ffe7238870475
Charles-Antoine <charto.bta@gmail.com>
2023-09-29 10:20:45 +0200

    v2.0

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/lilou.boutibonnes/OneDrive - JUNIA Grande école d'ingénieurs/Documents/GitHub/C/ex_invente
+# Install script for directory: /Users/charlesbta/Documents/GitHub/Jeu_pas_ouf
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ex_invente")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/lilou.boutibonnes/OneDrive - JUNIA Grande école d'ingénieurs/Documents/GitHub/C/ex_invente/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/charlesbta/Documents/GitHub/Jeu_pas_ouf/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -10,14 +10,28 @@ int jeu_nul(int n){
         return -3;
     } if (n==5){
         return 6;
-    } if (n==5){
+    } if (n==6){
         return 7;
     }
 }
-int main() {
+
+void Jeu(){
     int nb;
+    int result;
     printf("choisir un nombre entre 1 et 6.\n selecionner le bon nombre, si vous tombez sur un nombre inférieur à votre choix, vous gagnez");
     scanf("%d", &nb);
-    printf("%d", jeu_nul(nb));
+    result = jeu_nul(nb);
+    if(nb > result){
+        printf("%d\n",result);
+        printf("Victoire!");
+        return;
+    }
+    printf("%d\n",result);
+    printf("c'est perdu essaye encore\n");
+    Jeu();
+}
+
+int main() {
+    Jeu();
     return 0;
 }
